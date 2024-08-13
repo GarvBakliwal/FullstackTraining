5b5130229b9f08c5a68d0db624f50a994f19f71d
GarvBakliwal <garvbakliwal04@gmail.com>
2024-08-13 15:16:42 +0530

    class over

---
+++ Day07/index.js
@@ -0,0 +1,20 @@
+console.log("Garv");
+console.log("Bakliwal");
+// console.log(console);
+
+//value and variables ; 
+// data - types  ;
+
+// NOTE datatypes
+
+// NOTE PRIMITIVE
+
+// String
+// Number
+// BOOLEAN
+// NULL
+// UNDEFINED 
+// BigInt
+// Symbol
+ 
+// STRING 

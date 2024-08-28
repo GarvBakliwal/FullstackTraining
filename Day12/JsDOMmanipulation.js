228e6396a02e29f98232b867987c5bda29f45665
GarvBakliwal <garvbakliwal04@gmail.com>
2024-08-28 11:10:06 +0530

    Dom Manipulation Assignments 3, 4 & 5 Complete

--- Day12/JsDOMmanipulation.js
+++ Day12/JsDOMmanipulation.js
@@ -1,20 +1,20 @@
 // 1
-const translateButton = document.querySelector('#b1');
-const latinPara = document.querySelector('#p1');
+// const translateButton = document.querySelector('#b1');
+// const latinPara = document.querySelector('#p1');
 
-translateButton.onclick = function () {
-    latinPara.textContent = 'The company itself is a very successful company. Was he born to us in the most important times? Him by our services, or by the inventor of those who praise Him. Because the less things happen, the less things happen.';
-}
+// translateButton.onclick = function () {
+//     latinPara.textContent = 'The company itself is a very successful company. Was he born to us in the most important times? Him by our services, or by the inventor of those who praise Him. Because the less things happen, the less things happen.';
+// }
 
 
 // 2
-const colorChangingBox = document.getElementById('box');
-colorChangingBox.onmouseover = function () {
-    colorChangingBox.style.backgroundColor = 'lightblue';
-}
-colorChangingBox.onmouseout = function () {
-    colorChangingBox.style.backgroundColor = '';
-}
+// const colorChangingBox = document.getElementById('box');
+// colorChangingBox.onmouseover = function () {
+//     colorChangingBox.style.backgroundColor = 'lightblue';
+// }
+// colorChangingBox.onmouseout = function () {
+//     colorChangingBox.style.backgroundColor = '';
+// }
 // colorChangingBox.addEventListener('mouseover',()=> {
 //     colorChangingBox.style.backgroundColor = 'lightblue';
 // })
@@ -25,5 +25,38 @@ colorChangingBox.onmouseout = function () {
 
 
 // 3
-const unList = document.getElementById('unList');
-const addElementBtn = document.getElementById('addElementBtn');
+// const unList = document.getElementById('ulList');
+// const addElementBtn = document.getElementById('addElementBtn');
+// let count=0;
+
+// addElementBtn.onclick = function (){
+//     count++;
+//     const newListItem = document.createElement('li');
+//     newListItem.setAttribute('id',`listItem${count}`);
+//     newListItem.appendChild(document.createTextNode(`Element ${count}`));
+//     unList.appendChild(newListItem);
+// }
+
+
+// 4
+// const removeElementsBtn = document.getElementById('removeElementsBtn');
+// removeElementsBtn.onclick = function (){
+//     ListItem = document.getElementById(`listItem${count}`);
+//     unList.removeChild(ListItem);
+//     count--;
+// }
+
+
+// 5
+const inputForm = document.getElementById('inputForm');
+const inputName = document.getElementById('inputName');
+const inputEmail = document.getElementById('inputEmail');
+const submitBtn = document.getElementById('submitBtn');
+
+inputForm.addEventListener('submit',(event)=> {
+    event.preventDefault();
+    inputForm.style.display = "none";
+    const greetings = document.createElement('p');
+    greetings.innerText = `Thank you for submitting, ${inputName.value}`
+    document.body.append(greetings);
+})

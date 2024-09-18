fb4fa0fa5dca86d6902c043e3ecb01584a905acc
GarvBakliwal <garvbakliwal04@gmail.com>
2024-09-18 19:02:10 +0530

    Sample Project Login API complete

--- SampleProject/backend/controllers/userController.js
+++ SampleProject/backend/controllers/userController.js
@@ -1,5 +1,6 @@
 
 const User = require('./../models/userModel')
+const bcrypt = require('bcrypt')
 
 exports.register = async (req, res) => {
 
@@ -20,7 +21,26 @@ exports.register = async (req, res) => {
                 data: user
             })
         }
+    } catch (error) {
+        res.status(400).json({message:error});
+    }
+}
 
+exports.login = async (req,res)=>{
+    try {
+        const {email,password} = req.body;
+        const user = await User.findOne({email})
+        console.log(user);
+        if (!user) {
+            return res.status(400).send('User is not registered, Please register and try again')
+        }
+        const isPasswordMatch = await bcrypt.compare(password,user.password);
+        if (!isPasswordMatch) {
+            return res.status(400).send('Password do not Match')
+        }
+        res.status(200).json({
+            message:"Login Successful"
+        })
     } catch (error) {
         res.status(400).json({message:error});
     }

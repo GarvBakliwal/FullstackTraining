f0a7aebb3d398bc8bb81542333d6c51cdcff326c
GarvBakliwal <garvbakliwal04@gmail.com>
2024-09-27 13:46:38 +0530

    Product Update and Product Delete Task Complete

--- SampleProject/backend/controllers/productController.js
+++ SampleProject/backend/controllers/productController.js
@@ -39,3 +39,27 @@ exports.getProduct = async(req,res)=>{
         })
     }
 }
+
+exports.updateProduct = async (req, res) => {
+  const { id } = req.params;
+  // console.log("get body", req.body);
+  // console.log("get id", id);
+  try {
+      const updatedProduct = await Product.findByIdAndUpdate(id, req.body, {
+          new: true
+      });
+      res.status(200).json(updatedProduct);
+  } catch (error) {
+      res.status(400).send(error);
+  }
+}
+
+exports.deleteProduct = async (req, res) => {
+  const { id } = req.params;
+  try {
+      const deletedProduct = await Product.findByIdAndDelete(id);
+      res.status(200).json(deletedProduct);
+  } catch (error) {
+      res.status(400).send(error);
+  }
+}
--- SampleProject/backend/routes/productRoutes.js
+++ SampleProject/backend/routes/productRoutes.js
@@ -5,6 +5,8 @@ const productController = require('./../controllers/productController')
 const upload = require('./../middleware/upload')
 
 router.post('/addProduct', upload.single('image') , productController.createProduct)
-router.get('/getproducts', productController.getProduct)
+router.get('/getproducts', productController.getProduct);
+router.patch('/updateproduct/:id',productController.updateProduct);
+router.delete('/deleteproduct/:id',productController.deleteProduct);
 
 module.exports = router

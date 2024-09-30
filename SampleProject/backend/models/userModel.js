c738758e58c53fb27a48d7f811adcbbe040b29a0
GarvBakliwal <garvbakliwal04@gmail.com>
2024-09-30 15:59:15 +0530

    Dashboard Complete

--- SampleProject/backend/models/userModel.js
+++ SampleProject/backend/models/userModel.js
@@ -40,7 +40,7 @@ const userSchema = new mongoose.Schema({
   role : {
     type : String,
     enum : ['admin', 'user'],
-    default : 'user'
+    default : 'admin'
   },
   googleId : {
     type : String,
